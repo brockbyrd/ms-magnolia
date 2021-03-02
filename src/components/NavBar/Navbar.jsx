fc64ffe6efb42dec30b844a810fbf5ade3266ffb
brockbyrd <brockbyrdd@gmail.com>
2021-03-02 13:35:16 -0600

    adds search field to navbar

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography, InputBase } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
+import SearchIcon from '@material-ui/icons/Search';
 import { Link, useLocation } from 'react-router-dom';
 
 
@@ -9,7 +10,7 @@ import useStyles from './styles';
 
 const Navbar = () => {
     const classes = useStyles();
-    
+
     return(
         <>
         <AppBar position="fixed" className="classes.appBar" color="inherit">
@@ -18,6 +19,20 @@ const Navbar = () => {
                     <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Magnolia_sieboldii_flower_1.jpg/220px-Magnolia_sieboldii_flower_1.jpg' alt= "Hi" height="25px" className={classes.image}/>
                     Magnolia
                 </Typography>
+                <div className={classes.search}>
+                    <div className={classes.searchIcon}>
+                        <SearchIcon />
+                    </div>
+                        <InputBase
+                        placeholder="Search…"
+                        classes={{
+                            root: classes.inputRoot,
+                            input: classes.inputInput,
+                        }}
+                        inputProps={{ 'aria-label': 'search' }}
+                        />
+                </div>
+
                 <div className={classes.grow}/>
                 <div className={classes.button}>
                     <IconButton aria-label="Show Cart Items" color="inherit">
